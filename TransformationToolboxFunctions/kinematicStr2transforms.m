a4cba5614a3d22dee3a0f6a64c3998bd4c033efc
Michael Kutzer <kutzer@usna.edu>
2021-07-06 14:13:27 -0400

    Create kinematicStr2transforms.m

---
+++ TransformationToolboxFunctions/kinematicStr2transforms.m
@@ -0,0 +1,26 @@
+function tforms = kinematicStr2transforms(kStr)
+% KINEMATICSTR2TRANSFORMS isolates individual transforms from a string of
+% transformations
+%   tforms = KINEMATICSTR2TRANSFORMS(kStr)
+%
+%   Input(s)
+%       str - string of kinematic motion primitives (Tx, Ty, Tz, Rx, Ry,
+%             and Rz) each containing fixed values or variables. 
+%
+%   Output(s)
+%       tforms - cell array containing an ordered set of individual motion
+%                primitives (Tx, Ty, Tz, Rx, Ry, and Rz) contained in the 
+%                kinematic string. 
+%
+%   M. Kutzer, 06Jul2021, USNA
+
+%% Check inputs
+narginchk(1,1);
+% TODO - check input string
+ 
+%% Parse string
+idx = findstr(kStr,'*');
+idx = [0,idx,numel(kStr)+1]; % Append "first" and "last" index
+for i = 2:numel(idx)
+    tforms{i-1} = kStr( (idx(i-1)+1):(idx(i)-1) );
+end
