7623e5bb5981ad779584097db45456aee1d57103
Michael Kutzer <kutzer@usna.edu>
2022-09-26 14:29:33 -0400

    Update SE2toSE3.m

--- TransformationToolboxFunctions/SE2toSE3.m
+++ TransformationToolboxFunctions/SE2toSE3.m
@@ -1,8 +1,23 @@
 function SE3 = SE2toSE3(SE2)
-%SE2toSE3 converts a 2D homogeneous transformation into a 3D homogeneous
-%transformation assuming all rigid motions took place in the xy-plane.
+% SE2toSE3 converts a 2D homogeneous transformation into a 3D homogeneous
+% transformation assuming all rigid motions took place in the xy-plane.
+%   SE3 = SE2toSE3(SE2)
+%
+%   Input(s)
+%       SE2 - 3x3 array element of SE(2)
+%
+%   Output(s)
+%       SE3 - 4x4 array element of SE(3)
+%
+%   NOTES: 
+%       (1) This function does not check the validity of the input argument
+%           against the properties of SE(2). 
+%
+%   M. Kutzer, 16Feb2016, USNA
 
-SE3(4,4) = 1;
+% Update(s)
+%   26Sep2022 - Updated documentation
+
+SE3 = eye(4);
 SE3(1:2,1:2) = SE2(1:2,1:2);
-SE3(3,3) = 1;
 SE3(1:2,4) = SE2(1:2,3);
