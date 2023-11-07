c49636bdbeb58bd2a8bf1435087db94ba39c33c4
Michael Kutzer <kutzer@usna.edu>
2023-11-07 12:36:25 -0500

    Update randSE.m

--- TransformationToolboxFunctions/randSE.m
+++ TransformationToolboxFunctions/randSE.m
@@ -1,14 +1,38 @@
 function H = randSE(r,dim)
-% randSE creates a random homogeneous transformation element of SE(n) using
-% pseudorandom numbers.
-%   randSE creates a random 3D homogeneous transformation with a
-%   translation norm bounded by [0,1]
-%   randSE(r) creates a random 3D homogeneous transformation with a
-%   translation norm bounded by [0,r]
-%   randSE(r,dim) creates a random element of SE(n) with a translation norm
-%   bounded by [0,r]
+% randSE creates a random, n-dimensional homogeneous rigid body 
+% transformation element of SE(n).
 %
-%   See also randSO, wedge, vee, wedgeSO, veeSO
+%   Use Options:
+%
+%   OPTION 1: 
+%   H_a2b = randSE creates a random 3D homogeneous transformation with a
+%   translation norm bounded by [0,1] and rotation is generated using
+%   randSO.
+%
+%   H_a2b = randSE(r) creates a random 3D homogeneous transformation with a
+%   translation norm bounded by [0,r] and rotation is generated using
+%   randSO.
+%
+%   H_a2b = randSE(r,dim) creates a random N-D homogeneous transformation 
+%   (N = dim) with a translation norm bounded by [0,r] and rotation is 
+%   generated using randSO.
+%
+%   OPTION 2 (TODO - Complete Option 2)
+%   H_a2b = randSE(H_a2b_cov) generates a random sample from a 
+%   multivariate normal distribution (see mvnrnd) assuming a zero mean and 
+%   using the specified rigid body transformation covariance (see covSE).
+%
+%   H_a2b = randSE(H_a2b_mu,H_a2b_cov) generates a random sample from a 
+%   multivariate normal distribution (see mvnrnd) given a specified mean 
+%   (see meanSE) and covariance (see covSE).
+%
+%   H_a2b = randSE(___,n) allows the user to specify the number of samples
+%   returned. Samples will be returned in a cell array. 
+%
+%   [H_a2b,x_a2b,k_a2b] = randSE(___)
+%
+%   See also randSO, wedge, vee, wedgeSO, veeSO, wedgeSE, veeSE, meanSE,
+%   covSE
 %
 %   M. Kutzer 07July2017, USNA
 
