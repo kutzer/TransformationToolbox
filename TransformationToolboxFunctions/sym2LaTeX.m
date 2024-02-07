ed231ada1e0c569718af5843d5fa120c34d1e936
Michael Kutzer <kutzer@usna.edu>
2024-02-07 09:16:59 -0500

    Update sym2LaTeX.m

--- TransformationToolboxFunctions/sym2LaTeX.m
+++ TransformationToolboxFunctions/sym2LaTeX.m
@@ -106,9 +106,6 @@ end
 % -------------------------------------------------------------------------
 function [strNew,strPos,strSgn] = cs2str(ci,si,decStr)
 
-% TODO - support partial derivatives
-% TODO - allow eta to be replaced
-% etc.
 
 % Define coeffient
 if abs(ci) ~= 1
@@ -130,85 +127,8 @@ if matches(strSym,'1')
     return
 end
 
-% Replace known LaTeX math terms
-% -> Define term(s)
-symTerm = {...
-    '*',... % ----------------> * must come before Star
-    'sin','cos','tan','asin','acos','atan','log',...
-    'logm','Star',...
-    'alpha','beta','gamma','delta','epsilon','zeta','theta',...
-    'iota','kappa','lambda','mu','nu','xi','pi','rho','sigma',...
-    'tau','upsilon','chi','psi','omega'};%,...
-%'eta'}; % ----------------> eta is not currently supported.
-% -> Define replacement(s)
-texTerm = {...
-    ' ',... % ----------------> * must come before Star
-    '\sin','\cos','\tan','\asin','\acos','\atan','\log',...
-    '\log','*',...
-    '\alpha','\beta','\gamma','\delta','\epsilon','\zeta','\theta',...
-    '\iota','\kappa','\lambda','\mu','\nu','\xi','\pi','\rho','\sigma',...
-    '\tau','\upsilon','\chi','\psi','\omega'};%,...
-%'\eta'}; % ----------------> eta is not currently supported.
-% -> Perform the replacements
-for i = 1:numel(texTerm)
-    strSym = strrep(strSym, symTerm{i}, texTerm{i});
-end
-
-% Replace superscript & subscript terms
-% -> Define the pattern
-pattern = '([A-Za-z\d]+)_([A-Za-z\d\*]+)To([A-Za-z\d\*]+)';
-% -> Define replacement
-replacement = '$1_{$2}^{$3}';
-% -> Perform the replacement using regular expression
-strSym = regexprep(strSym, pattern, replacement);
-
-% Replace subscript only terms
-% -> Define the pattern
-pattern = '([A-Za-z\d]+)_([A-Za-z\d]+)';
-% -> Define replacement
-replacement = '$1_{$2}';
-% -> Perform the replacement using regular expression
-strSym = regexprep(strSym, pattern, replacement);
-
-% Replace superscript only terms
-% -> Define the pattern
-pattern = '([A-Za-z\d]+)_To([A-Za-z\d\*]+)';
-% -> Define replacement
-replacement = '$1^{$2}';
-% -> Perform the replacement using regular expression
-strSym = regexprep(strSym, pattern, replacement);
-
-% Account for special cases (sqrt)
-% TODO - This doesn't work!
-%{
-% -> Define the pattern
-pattern = '\((.*?)\)\^\(1/2\)';
-% -> Define the replacement
-replacement = '\\sqrt{$1}';
-% -> Perform the replacement using regular expression
-strSym = regexprep(strSym, pattern, replacement);
-%}
-
-% Account for fractions (this should not be the case)
-% TODO - This doesn't work!
-%{
-% -> Define the pattern
-pattern = '([^/]+)/([^/]+)';
-% -> Define the replacement
-replacement = '\\frac{$1}{$2}';
-% -> Perform the replacement using regular expression
-strSym = regexprep(strSym, pattern, replacement);
-%}
-
-% Replace remaining terms
-% -> Define term(s)
-symTerm = {'(',')','[',']'};
-% -> Define replacement(s)
-texTerm = {'\left(','\right)','\left[','\right]'};
-% -> Perform the replacements
-for i = 1:numel(texTerm)
-    strSym = strrep(strSym, symTerm{i}, texTerm{i});
-end
+% Process symbolic string
+strSym = str2LaTeX(strSym);
 
 % Combine strings
 strNew = sprintf('%s%s',strNew,strSym);
