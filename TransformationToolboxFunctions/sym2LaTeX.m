a2af1b96f982add071645440452b1e390e791bca
Michael Kutzer <kutzer@usna.edu>
2024-02-07 08:58:46 -0500

    Update sym2LaTeX.m

--- TransformationToolboxFunctions/sym2LaTeX.m
+++ TransformationToolboxFunctions/sym2LaTeX.m
@@ -15,6 +15,7 @@ function str = sym2LaTeX(val,dec)
 %           convention. For example:
 %               H_1To0 --> H_{1}^{0}
 %               H_To0  --> H^{0}
+%               R_aToStar --> R_{a}^{*}
 %       (2) Modifiers such as bar, underline, vec, etc can be added using
 %           camel case text prior to the variable. For example:
 %               vecUnderlineB_To6 --> \vec{\underline{B}}^{6}
@@ -42,48 +43,60 @@ decStr = ['%','.',num2str(dec),'f'];
 %% Convert terms
 str = '';
 if strcmpi( class(val), 'sym') % apply for symbolic inputs
-    
+
     % Isolate numerator and denominator
     [n,d] = numden(val); % account for expressions that are fractions
     
-    % Account for special case (numerator of 0)
     if n == 0
+        % Account for special case (numerator of 0)
         str = sprintf(decStr,n);
-        return
-    end
-
-    % Numerator
-    [c,s] = coeffs(n);
-    if ~isempty(c) && ~isempty(s)
-        for k = 1:numel(c)
-            % Convert symbolic expression to latex
-            [~,strPos,strSgn] = cs2str(c(k),s(k),decStr);
-            
-            % Append string
-            str = appendNewString(str,strPos,strSgn);
-        end
     else
-        n = 0;
-    end
+        % Account for standard case 
 
-    % Denominator
-    if d ~= 1
-        strD = '';
-        [c,s] = coeffs(d);
-        for k = 1:numel(c)
-            % Convert symbolic expression to latex
-            [~,strPos,strSgn] = cs2str(c(k),s(k),decStr);
-            
-            % Append string
-            strD = appendNewString(strD,strPos,strSgn);
+        % Numerator
+        [c,s] = coeffs(n);
+        if ~isempty(c) && ~isempty(s)
+            for k = 1:numel(c)
+                % Convert symbolic expression to latex
+                [~,strPos,strSgn] = cs2str(c(k),s(k),decStr);
+
+                % Append string
+                str = appendNewString(str,strPos,strSgn);
+            end
+        else
+            n = 0;
         end
-        str = sprintf('\\frac{%s}{%s}',str,strD);
-    end
 
+        % Denominator
+        if d ~= 1
+            strD = '';
+            [c,s] = coeffs(d);
+            for k = 1:numel(c)
+                % Convert symbolic expression to latex
+                [~,strPos,strSgn] = cs2str(c(k),s(k),decStr);
+
+                % Append string
+                strD = appendNewString(strD,strPos,strSgn);
+            end
+            str = sprintf('\\frac{%s}{%s}',str,strD);
+        end
+
+    end
 else
 
     str = sprintf(decStr,val);
 
+end
+
+%% Account for values of 0 and 1
+strZero = sprintf(decStr,0);
+if matches(str,strZero)
+    str = '0';
+end
+
+strOne  = sprintf(decStr,1);
+if matches(str,strOne)
+    str = '1';
 end
 
 end
@@ -94,7 +107,7 @@ end
 function [strNew,strPos,strSgn] = cs2str(ci,si,decStr)
 
 % TODO - support partial derivatives
-% TODO - support ^* terms
+% TODO - allow eta to be replaced
 % etc.
 
 % Define coeffient
@@ -110,23 +123,42 @@ strSgn = sign(ci);
 % Convert symbolic term to string
 strSym = char(si);
 
+% Account for special case (si == 1)
+if matches(strSym,'1')
+    strNew = sprintf(decStr,ci);
+    strPos = sprintf(decStr,abs(ci));
+    return
+end
+
 % Replace known LaTeX math terms
 % -> Define term(s)
-symTerm = {'sin','cos','tan','asin','acos','atan','log',...
-    'logm'};
+symTerm = {...
+    '*',... % ----------------> * must come before Star
+    'sin','cos','tan','asin','acos','atan','log',...
+    'logm','Star',...
+    'alpha','beta','gamma','delta','epsilon','zeta','theta',...
+    'iota','kappa','lambda','mu','nu','xi','pi','rho','sigma',...
+    'tau','upsilon','chi','psi','omega'};%,...
+%'eta'}; % ----------------> eta is not currently supported.
 % -> Define replacement(s)
-texTerm = {'\sin','\cos','\tan','\asin','\acos','\atan','\log',...
-    '\log'};
+texTerm = {...
+    ' ',... % ----------------> * must come before Star
+    '\sin','\cos','\tan','\asin','\acos','\atan','\log',...
+    '\log','*',...
+    '\alpha','\beta','\gamma','\delta','\epsilon','\zeta','\theta',...
+    '\iota','\kappa','\lambda','\mu','\nu','\xi','\pi','\rho','\sigma',...
+    '\tau','\upsilon','\chi','\psi','\omega'};%,...
+%'\eta'}; % ----------------> eta is not currently supported.
 % -> Perform the replacements
-for i = 1:length(texTerm)
+for i = 1:numel(texTerm)
     strSym = strrep(strSym, symTerm{i}, texTerm{i});
 end
 
 % Replace superscript & subscript terms
 % -> Define the pattern
-pattern = '([A-Za-z\d]+)_([A-Za-z\d]+)To([A-Za-z\d]+)';
+pattern = '([A-Za-z\d]+)_([A-Za-z\d\*]+)To([A-Za-z\d\*]+)';
 % -> Define replacement
-replacement = '$1_{\2}^{\3}';
+replacement = '$1_{$2}^{$3}';
 % -> Perform the replacement using regular expression
 strSym = regexprep(strSym, pattern, replacement);
 
@@ -134,15 +166,15 @@ strSym = regexprep(strSym, pattern, replacement);
 % -> Define the pattern
 pattern = '([A-Za-z\d]+)_([A-Za-z\d]+)';
 % -> Define replacement
-replacement = '$1_{\2}';
+replacement = '$1_{$2}';
 % -> Perform the replacement using regular expression
 strSym = regexprep(strSym, pattern, replacement);
 
 % Replace superscript only terms
 % -> Define the pattern
-pattern = '([A-Za-z\d]+)_To([A-Za-z\d]+)';
+pattern = '([A-Za-z\d]+)_To([A-Za-z\d\*]+)';
 % -> Define replacement
-replacement = '$1^{\2}';
+replacement = '$1^{$2}';
 % -> Perform the replacement using regular expression
 strSym = regexprep(strSym, pattern, replacement);
 
@@ -168,13 +200,13 @@ replacement = '\\frac{$1}{$2}';
 strSym = regexprep(strSym, pattern, replacement);
 %}
 
-% Replace remaining terms 
+% Replace remaining terms
 % -> Define term(s)
-symTerm = {'(',')','[',']','*'};
+symTerm = {'(',')','[',']'};
 % -> Define replacement(s)
-texTerm = {'\left(','\right)','\left[','\right]',' '};
+texTerm = {'\left(','\right)','\left[','\right]'};
 % -> Perform the replacements
-for i = 1:length(texTerm)
+for i = 1:numel(texTerm)
     strSym = strrep(strSym, symTerm{i}, texTerm{i});
 end
 
@@ -185,7 +217,7 @@ strPos = sprintf('%s%s',strPos,strSym);
 % Account for empty strSym
 if isempty(strSym)
     strNew = sprintf(decStr,ci);
-    strPos = sprintf(decStr,abs(ci));   
+    strPos = sprintf(decStr,abs(ci));
 end
 
 end
