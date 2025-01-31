47966b5dabd726d991d844f9a3641e3d85ebf025
Michael Kutzer <kutzer@usna.edu>
2025-01-31 15:00:13 -0500

    Update SCRIPT_plotJoint.m

--- TransformationToolbox Example SCRIPTS/SCRIPT_plotJoint.m
+++ TransformationToolbox Example SCRIPTS/SCRIPT_plotJoint.m
@@ -4,14 +4,31 @@ close all
 clc
 
 %% Create a 3D RRR arm
+% Define figure and axes
 fig = figure;
 axs = axes('Parent',fig,'NextPlot','add','DataAspectRatio',[1 1 1]);
+view(axs,3);
 
+% Define base frame
 h_o2a = triad('Parent',axs);
 
-[h_1to0,jnt1] = plotJoint(h_o2a,'Revolute',[0,0,1],0.5);
-h_Lto1 = triad('Matrix',Tz(3),'Parent',h_1to0);
-[h_2toL,jnt2] = plotJoint(h_Lto1,'Revolute',[0,1,0],0.5);
-h_Lto2 = triad('Matrix',Tx(3),'Parent',h_2toL);
-[h_3toL,jnt3] = plotJoint(h_Lto2,'Revolute',[0,1,0],0.5);
-h_Lto3 = triad('Matrix',Tx(3),'Parent',h_3toL);
+% Joint 1
+[h_J1to0,jnt1] = plotJoint(h_o2a,'Revolute',[0,0,1],0.5);
+% Link 1
+H_L1toJ1 = Tz(3)*Rx(pi/2);
+h_L1toJ1 = triad('Matrix',H_L1toJ1,'Parent',h_J1to0);
+lnk1 = plotLink(h_J1to0,H_L1toJ1(1:3,4),[0.5,0.5]);
+
+% Joint 2
+[h_J2toL1,jnt2] = plotJoint(h_L1toJ1,'Revolute',[0,0,1],0.5);
+% Link 2
+H_L2toJ2= Tx(3);
+h_L2toJ2 = triad('Matrix',H_L2toJ2,'Parent',h_J2toL1);
+lnk2 = plotLink(h_J2toL1,H_L2toJ2(1:3,4),[0.5,0.5]);
+
+% Joint 3
+[h_J3toL2,jnt3] = plotJoint(h_L2toJ2,'Revolute',[0,1,0],0.5);
+% Link 3
+H_L3toJ3= Tx(3);
+h_L3toJ3 = triad('Matrix',H_L3toJ3,'Parent',h_J3toL2);
+lnk3 = plotLink(h_J3toL2,H_L3toJ3(1:3,4),[0.5,0]);
