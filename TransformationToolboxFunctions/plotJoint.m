d1f2f07420950202f4d45b7717c1ff9fb19768cf
Michael Kutzer <kutzer@usna.edu>
2025-01-31 13:10:26 -0500

    Update plotJoint.m

--- TransformationToolboxFunctions/plotJoint.m
+++ TransformationToolboxFunctions/plotJoint.m
@@ -9,12 +9,12 @@ function [h_j2p,fcn] = plotJoint(varargin)
 %       axs    - [OPTIONAL] handle defining the parent of the plotted joint
 %       jType  - Joint type {'Revolute','Prismatic'}
 %       jAxis  - 3x1 unit vector defining positive axis of translation or
-%                rotation for the joint 
+%                rotation for the joint
 %       jScale - positive scalar defining scale of joint
-%       
+%
 %   Output(s)
 %       h_j2p - hgtransform object defining the "joint frame" relative to
-%               the parent frame. 
+%               the parent frame.
 %       fcn   - anonymous function to rotate or translate the joint
 %
 %   M. Kutzer, 31Jan2025, USNA
@@ -48,19 +48,19 @@ end
 
 % Define type
 if nargin >= nargin_i
-	jType = lower( varargin{nargin_i} );
+    jType = lower( varargin{nargin_i} );
     nargin_i = nargin_i+1;
 end
 
 % Define axis
 if nargin >= nargin_i
-	jAxis = varargin{nargin_i};
+    jAxis = varargin{nargin_i};
     nargin_i = nargin_i+1;
 end
 
 % Define scale
 if nargin >= nargin_i
-	jScale = varargin{nargin_i};
+    jScale = varargin{nargin_i};
     nargin_i = nargin_i+1;
 end
 
@@ -95,17 +95,17 @@ switch jType
 
         % Plot cylinder
         [ptc_cy,plt_cy] = plotCylinder(axs,cyfit);
-        
+
         % Adjust plot object settings
         set(ptc_cy,'FaceColor','k','FaceAlpha',0.5,'EdgeColor','none');
         set(plt_cy,'Color','k','LineWidth',2);
 
         % Create function
-        fcn = @(theta) set(h_j2p,'Matrix',expm(wedgeSE([jAxis*theta; zeros(3,1)])));
+        fcn = @(theta) revoluteJnt(h_j2p,jAxis,theta);
     case 'prismatic'
         % Define orientation
         R = vectorToSO(jAxis,3);
-        
+
         % Define cube
         cufit.Center = zeros(3,1);
         cufit.Rotation = R;
@@ -115,7 +115,7 @@ switch jType
         ptc_cu = plotCube(axs,cufit);
 
         % Define square
-        sqfit.Center = 1.1*jAxis*jScale;
+        sqfit.Center = 1.05*jAxis*jScale;
         sqfit.Rotation = R;
         sqfit.Dimension = jScale;
 
@@ -126,8 +126,31 @@ switch jType
         set(ptc_cu,'FaceColor','k','FaceAlpha',0.5,'EdgeColor','k','LineWidth',2);
         set(ptc_sq,'FaceColor','k','FaceAlpha',0.5,'EdgeColor','k','LineWidth',2);
 
+        % Plot length
+        plt_ln = plot(axs,nan,nan,'k','LineWidth',2);
+
         % Create function
-        fcn = @(d) set(h_j2p,'Matrix',Tx(jAxis(1)*d)*Ty(jAxis(2)*d)*Tz(jAxis(3)*d));
+        fcn = @(d) prismaticJnt(h_j2p,plt_ln,jAxis,jScale,d);
     otherwise
         error('Joint type must be revolute or prismatic.');
+end
+
+end
+
+% Internal function(s)
+
+function revoluteJnt(h_j2p,jAxis,theta)
+
+set(h_j2p,'Matrix',expm(wedgeSE([jAxis*theta; zeros(3,1)])));
+
+end
+
+function prismaticJnt(h_j2p,plt_ln,jAxis,jScale,d)
+
+set(h_j2p,'Matrix',Tx(jAxis(1)*d)*Ty(jAxis(2)*d)*Tz(jAxis(3)*d));
+set(plt_ln,...
+    'XData',[1.05*jAxis(1)*jScale, jAxis(1)*d],...
+    'YData',[1.05*jAxis(2)*jScale, jAxis(2)*d],...
+    'ZData',[1.05*jAxis(3)*jScale, jAxis(3)*d]);
+
 end
