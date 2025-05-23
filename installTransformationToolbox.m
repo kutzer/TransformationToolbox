b1ae3a8ef67b7ba4246bcc3c0d9e2d28a28c01a6
Michael Kutzer <kutzer@usna.edu>
2025-05-23 11:46:26 -0400

    Update installTransformationToolbox.m

--- installTransformationToolbox.m
+++ installTransformationToolbox.m
@@ -16,16 +16,50 @@ function installTransformationToolbox(replaceExisting)
 
 % Updates
 %   08Jan2021 - Corrected questdlg
-
-% TODO - Allow users to create a local version if admin rights are not
-% possible.
+%   22May2025 - Enable local user installation
 
 %% Assign tool/toolbox specific parameters
 dirName = 'transformation';
+toolboxContent  = 'TransformationToolboxFunctions';
+toolboxExamples = 'TransformationToolbox Example SCRIPTS';
+toolboxName = 'Transformation Toolbox';
+toolboxShort = strrep(toolboxName, ' ', '');
+
+%% Define toolbox directory options
+toolboxPathAdmin = fullfile(matlabroot,'toolbox',dirName);
+toolboxPathLocal = fullfile(prefdir,'toolbox',dirName);
+toolboxPathExamples = fullfile(userpath,sprintf('Examples, %s',toolboxName));
+
+%% Check if folders exist
+isPathAdmin = isfolder(toolboxPathAdmin);
+isPathLocal = isfolder(toolboxPathLocal);
+isPathExamples = isfolder(toolboxPathExamples);
+
+%% Check if folders are in the MATLAB path
+allPaths = path;
+allPaths = strsplit(allPaths,pathsep);
+
+inPathAdmin = any(matches(allPaths,toolboxPathAdmin),'all');
+inPathLocal = any(matches(allPaths,toolboxPathLocal),'all');
+inPathExamples = any(matches(allPaths,toolboxPathExamples),'all');
 
 %% Check inputs
-if nargin == 0
+if nargin < 1
     replaceExisting = [];
+end
+if nargin < 2
+    skipAdmin = false;
+end
+
+%% Check for admin write access
+isAdmin = checkWriteAccess(matlabroot);
+isLocal = checkWriteAccess(prefdir);
+isExample = checkWriteAccess(userpath);
+
+%% Check for basic write access
+if ~isLocal
+    warning('No local write access?');
+    return
 end
 
 %% Installation error solution(s)
@@ -37,35 +71,20 @@ adminSolution = sprintf(...
      '\t\t(b) Right click\n',...
      '\t\t(c) Select "Run as administrator"\n']);
 
-%% Check for toolbox directory
-toolboxRoot  = fullfile(matlabroot,'toolbox',dirName);
-isToolbox = exist(toolboxRoot,'file');
-if isToolbox == 7
-    % Apply replaceExisting argument
-    if isempty(replaceExisting)
-        choice = questdlg(sprintf(...
-            ['MATLAB Root already contains the Transformation Toolbox.\n',...
-            'Would you like to replace the existing toolbox?']),...
-            'Replace Existing Transformation Toolbox','Yes','No','Cancel','Yes');
-    elseif replaceExisting
-        choice = 'Yes';
-    else
-        choice = 'No';
-    end
+%% Prompt user if not an admin
+if ~isAdmin && ~skipAdmin
+    choice = questdlg(sprintf(...
+        ['MATLAB is running without administrative privileges.\n',...
+        'Would you like to install the %s locally?'],toolboxName),...
+        sprintf('Local Install %s',toolboxName),...
+        'Yes','No','Cancel','Yes');
     % Replace existing or cancel installation
     switch choice
         case 'Yes'
-            rmpath(toolboxRoot);
-            [isRemoved, msg, msgID] = rmdir(toolboxRoot,'s');
-            if isRemoved
-                fprintf('Previous version of Transformation Toolbox removed successfully.\n');
-            else
-                fprintf('Failed to remove old Transformation Toolbox folder:\n\t"%s"\n',toolboxRoot);
-                fprintf(adminSolution);
-                error(msgID,msg);
-            end
+            skipAdmin = true;
         case 'No'
-            fprintf('Transformation Toolbox currently exists, installation cancelled.\n');
+            fprintf('Unable to write perform installation.\n\n');
+            fprintf('To install as an administrator - %s',adminSolution);
             return
         case 'Cancel'
             fprintf('Action cancelled.\n');
@@ -75,41 +94,123 @@ if isToolbox == 7
     end
 end
 
-%% Create Scorbot Toolbox Path
-[isDir,msg,msgID] = mkdir(toolboxRoot);
-if isDir
-    fprintf('Transformation toolbox folder created successfully:\n\t"%s"\n',toolboxRoot);
+%% Check for existing toolbox
+if skipAdmin
+    isToolbox = isPathLocal;
+    toolboxPath = toolboxPathLocal;
 else
-    fprintf('Failed to create Scorbot Toolbox folder:\n\t"%s"\n',toolboxRoot);
-    fprintf(adminSolution);
-    error(msgID,msg);
+    isToolbox = isPathAdmin;
+    toolboxPath = toolboxPathAdmin;
+end
+
+%% Check for toolbox directory
+if isToolbox
+    % Apply replaceExisting argument
+    if isempty(replaceExisting)
+        choice = questdlg(sprintf(...
+            ['MATLAB path already contains the %s.\n',...
+            'Would you like to replace the existing toolbox?'],toolboxName),...
+            sprintf('Replace Existing %s',toolboxName),...
+            'Yes','No','Cancel','Yes');
+    elseif replaceExisting
+        choice = 'Yes';
+    else
+        choice = 'No';
+    end
+    % Replace existing or cancel installation
+    switch choice
+        case 'Yes'
+            % Remove existing paths
+            removePath(toolboxName,...
+                toolboxPathAdmin,inPathAdmin,isPathAdmin,isAdmin);
+            removePath(toolboxName,...
+                toolboxPathLocal,inPathLocal,isPathLocal,isLocal);
+            removePath(toolboxName,...
+                toolboxPathExamples,inPathExamples,inPathExamples,isExample);
+        case 'No'
+            fprintf('%s currently exists, installation cancelled.\n',toolboxName);
+            return
+        case 'Cancel'
+            fprintf('Action cancelled.\n');
+            return
+        otherwise
+            error('Unexpected response.');
+    end
 end
 
 %% Migrate toolbox folder contents
-toolboxContent = 'TransformationToolboxFunctions';
-if ~isdir(toolboxContent)
+% Toolbox contents
+migrateContent(toolboxContent,toolboxPath,toolboxName);
+% Example files
+try
+migrateContent(toolboxExamples,toolboxPathExamples,...
+    sprintf('%s Examples',toolboxName));
+catch
+    fprintf('Unable to migrate examples.');
+end
+
+%% Save toolbox path
+%addpath(genpath(toolboxRoot),'-end');
+addpath(toolboxPath,'-end');
+pathdef_local = fullfile(userpath,'pathdef.m');
+if isAdmin
+    % Delete local pathdef file
+    if isfile(pathdef_local)
+        delete(pathdef_local);
+    end
+    % Save administrator local pathdef file
+    savepath;
+else
+    % Create local user pathdef file
+    fprintf('Updating local user "pathdef.m"...')
+    savepath( pathdef_local );
+    fprintf('[Complete]\n');
+end
+
+%% Rehash toolbox cache
+fprintf('Rehashing Toolbox Cache...');
+rehash TOOLBOXCACHE
+fprintf('[Complete]\n');
+
+%% internal functions (shared workspace)
+% ------------------------------------------------------------------------
+function migrateContent(sourceIn,destination,msg)
+
+% Migrate toolbox folder contents
+if ~isfolder(sourceIn)
     error(sprintf(...
-        ['Change your working directory to the location of "installTransformationToolbox.m".\n',...
+        ['Change your working directory to the location of "install%s.m".\n',...
          '\n',...
          'If this problem persists:\n',...
-         '\t(1) Unzip your original download of "TransformationToolbox" into a new directory\n',...
+         '\t(1) Unzip your original download of "%s" into a new directory\n',...
          '\t(2) Open a new instance of MATLAB "as administrator"\n',...
          '\t\t(a) Locate MATLAB shortcut\n',...
          '\t\t(b) Right click\n',...
          '\t\t(c) Select "Run as administrator"\n',...
-         '\t(3) Change your "working directory" to the location of "installTransformationToolbox.m"\n',...
-         '\t(4) Enter "installTransformationToolbox" (without quotes) into the command window\n',...
-         '\t(5) Press Enter.']));
+         '\t(3) Change your "working directory" to the location of "install%s.m"\n',...
+         '\t(4) Enter "install%s" (without quotes) into the command window\n',...
+         '\t(5) Press Enter.'],toolboxShort,toolboxShort,toolboxShort,toolboxShort));
 end
-files = dir(toolboxContent);
-wb = waitbar(0,'Copying Transformation Toolbox toolbox contents...');
+
+% Create Toolbox Path
+[isDir,msgDir,msgID] = mkdir(destination);
+if isDir
+    fprintf('%s folder created successfully:\n\t"%s"\n',msg,destination);
+else
+    fprintf('Failed to create %s folder:\n\t"%s"\n',msg,destination);
+    fprintf(adminSolution);
+    error(msgID,msgDir);
+end
+
+% Migrate contents
+files = dir(sourceIn);
+wb = waitbar(0,sprintf('Copying %s contents...',msg));
 n = numel(files);
-fprintf('Copying Transformation Toolbox contents:\n');
+fprintf('Copying %s contents:\n',msg);
 for i = 1:n
     % source file location
-    source = fullfile(toolboxContent,files(i).name);
-    % destination location
-    destination = toolboxRoot;
+    source = fullfile(sourceIn,files(i).name);
+    
     if files(i).isdir
         switch files(i).name
             case '.'
@@ -156,13 +257,45 @@ for i = 1:n
     waitbar(i/n,wb);
 end
 set(wb,'Visible','off');
+delete(wb);
 
-%% Save toolbox path
-%addpath(genpath(toolboxRoot),'-end');
-addpath(toolboxRoot,'-end');
-savepath;
+end
 
-%% Rehash toolbox cache
-fprintf('Rehashing Toolbox Cache...');
-rehash TOOLBOXCACHE
-fprintf('[Complete]\n');
+end
+
+%% Internal functions (unique workspace)
+% ------------------------------------------------------------------------
+function tfWrite = checkWriteAccess(pname)
+
+tmpFname = fullfile(pname,'tmp.txt');
+tmpHndle = fopen(tmpFname, 'w');
+if tmpHndle < 0
+    tfWrite = false;
+else
+    tfWrite = true;
+    fclose(tmpHndle);
+    delete(tmpFname);
+end
+
+end
+% ------------------------------------------------------------------------
+function removePath(toolboxName,pName,inPath,isPath,isDelete)
+% Remove path
+if inPath
+    rmpath(pName);
+end
+% Remove folder
+if isPath && isDelete
+    [isRemoved, msg, msgID] = rmdir(pName,'s');
+    if isRemoved
+        fprintf('Previous version of %s removed successfully:\n\t"%s"\n',toolboxName,pName);
+    else
+        fprintf('Failed to remove old %s folder:\n\t"%s"\n',toolboxName,pName);
+        %fprintf(adminSolution);
+        error(msgID,msg);
+    end
+elseif ~isDelete
+    fprintf('Skipping removal of old %s folder:\n\t"%s"\n',toolboxName,pName);
+end
+
+end
